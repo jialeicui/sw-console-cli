73d9cbbb71634b4b0decfeee29a58673d6b578b9
Jialei <jialeicui@126.com>
2022-11-03 15:43:26 +0800

    output es module

--- src/webpack.ts
+++ src/webpack.ts
@@ -1,6 +1,7 @@
 import { Configuration } from 'webpack'
 import { join } from 'path'
 import { readdirSync } from 'fs'
+import CopyWebpackPlugin from 'copy-webpack-plugin'
 
 const getEntry = (dir: string) => {
     const files = readdirSync(dir).filter((file) => {
@@ -15,6 +16,11 @@ const getEntry = (dir: string) => {
     }
 }
 
+// TODO read from manifest.json
+const pluginName = () => {
+    return 'foo'
+}
+
 export const getWebpackConf = (): Configuration => {
     const dir = join(process.cwd(), 'src')
     return {
@@ -24,14 +30,73 @@ export const getWebpackConf = (): Configuration => {
         entry: getEntry(dir),
         output: {
             path: join(process.cwd(), 'dist'),
+            library: {
+                type: 'module',
+            },
             filename: '[name].js',
         },
+        experiments: {
+            outputModule: true,
+        },
+        plugins: [
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: 'img/**/*',
+                        to: '.',
+                        noErrorOnMissing: true,
+                    },
+                ],
+            }),
+        ],
         module: {
             rules: [
                 {
-                    test: /\.tsx?$/,
-                    use: 'ts-loader',
+                    test: /\.[tj]sx?$/,
+                    use: {
+                        loader: require.resolve('babel-loader'),
+                        options: {
+                            cacheDirectory: true,
+                            cacheCompression: false,
+                            presets: [
+                                [require.resolve('@babel/preset-env'), { modules: false }],
+                                [
+                                    require.resolve('@babel/preset-typescript'),
+                                    {
+                                        allowNamespaces: true,
+                                        allowDeclareFields: true,
+                                    },
+                                ],
+                                [require.resolve('@babel/preset-react')],
+                            ],
+                            plugins: [
+                                [
+                                    require.resolve('@babel/plugin-transform-typescript'),
+                                    {
+                                        allowNamespaces: true,
+                                        allowDeclareFields: true,
+                                    },
+                                ],
+                            ],
+                        },
+                    },
                     exclude: /node_modules/,
+                },
+                {
+                    test: /\.(png|jpe?g|gif|svg)$/,
+                    type: 'asset/resource',
+                    generator: {
+                        publicPath: `public/plugins/${pluginName()}/img/`,
+                        outputPath: 'img/',
+                    },
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)(\?v=\d+\.\d+\.\d+)?$/,
+                    type: 'asset/resource',
+                    generator: {
+                        publicPath: `public/plugins/${pluginName()}/fonts/`,
+                        outputPath: 'fonts/',
+                    },
                 },
             ],
         },
@@ -40,16 +105,6 @@ export const getWebpackConf = (): Configuration => {
             modules: [dir, 'node_modules'],
         },
         externals: [
-            'lodash',
-            'moment',
-            'react',
-            'react-dom',
-            'react-redux',
-            'redux',
-            'rxjs',
-            'react-router',
-            'react-router-dom',
-            'd3',
             ({ request }, callback) => {
                 const prefix = 'starwhale/'
                 if (request?.indexOf(prefix) === 0) {
